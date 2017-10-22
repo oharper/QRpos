a0df0df4903769d0ee83c99d25c6e6b831802e68
Owen <oharper20@gmail.com>
2017-10-22 18:51:54 +0100

    Created all views in storyboard, created an initial view controller with rounded button method, also created custom cells and their files.

---
+++ QRpos/Cells/OrderTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  OrderTableViewCell.swift
+//  QRpos
+//
+//  Created by Owen Harper on 22/10/2017.
+//  Copyright © 2017 Owen Harper. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OrderCell: UITableViewCell {
+  
+  @IBOutlet weak var name: UILabel!
+  @IBOutlet weak var itemPrice: UILabel!
+  @IBOutlet weak var itemTotal: UILabel!
+  
+  
+}
--- QRpos/InitialView.swift
+++ QRpos/InitialView.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var newOrderButton: UIButton!
+  @IBOutlet weak var closeTabButton: UIButton!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //Rounds the corners of the action buttons
+    newOrderButton.layer.cornerRadius = 6
+    newOrderButton.clipsToBounds = true
+    closeTabButton.layer.cornerRadius = 6
+    closeTabButton.clipsToBounds = true
+    
   }
 
   override func didReceiveMemoryWarning() {
