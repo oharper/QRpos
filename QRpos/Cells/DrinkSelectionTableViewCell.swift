edc0ba201935595171c059bd3cfe9da2c22a3d78
Owen <oharper20@gmail.com>
2017-10-22 22:01:18 +0100

    Files created for cells

---
+++ QRpos/Cells/DrinkSelectionTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DrinkSelectionTableViewCell.swift
+//  QRpos
+//
+//  Created by Owen Harper on 22/10/2017.
+//  Copyright © 2017 Owen Harper. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DrinkSelectionCell: UITableViewCell {
+  
+  @IBOutlet weak var name: UILabel!
+  @IBOutlet weak var price: UILabel!
+  
+  
+}
