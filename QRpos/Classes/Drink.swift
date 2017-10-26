265e83f6e5fd60f91de2f5b0ec87c7c6a0990efa
Owen <oharper20@gmail.com>
2017-10-26 18:53:38 +0100

    Added drink and order item classes to version control

---
+++ QRpos/Classes/Drink.swift
@@ -0,0 +1,23 @@
+//
+//  Drink.swift
+//  QRpos
+//
+//  Created by Owen Harper on 25/10/2017.
+//  Copyright © 2017 Owen Harper. All rights reserved.
+//
+
+import UIKit
+
+class Drink {
+  
+  var name: String
+  var price: Double
+  
+  
+  init(Name:String, Price:Double) {
+    
+    self.name = Name
+    self.price = Price
+    
+}
+}
